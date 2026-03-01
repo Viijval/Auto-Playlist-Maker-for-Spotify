34462b01b965f6f2d5425b4a095b69af806b0d04
Viijval <159607092+Viijval@users.noreply.github.com>
2026-03-02 00:50:25 +0530

    Update main.py

--- autoplaylist-backend/main.py
+++ autoplaylist-backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     
     #allow_origins=["http://localhost:5173"],
-    allow_origins=["https://auto-playlist-maker-for-spotify.vercel.app/"],
+    allow_origins=["https://auto-playlist-maker-for-spotify.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
